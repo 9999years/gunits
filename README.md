5122199c26d38bf16ecca0ddd29fe98beecff3a6
Rebecca Turner <637275@gmail.com>
2017-12-16 13:27:03 -0500

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+My copy of [GNU units][units] had currency definitions a decade old! You know
+what never has that and does unit conversions? Google! Here’s an always-online
+command-line alternative to GNU units, recommended only for fluxuating units
+such as currency conversions.
+
+Build:
+
+    dotnet build -c release
+
+Use:
+
+[units]: https://www.gnu.org/software/units/
---
+++ gunits.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Web;
+using HtmlAgilityPack;
+
+namespace gunits
+{
+	class GUnits
+	{
+		static string[] Lines(string input) {
+			return input.Split('\n');
+		}
+
+		static HtmlNode SearchNode(HtmlDocument doc) {
+			return doc.DocumentNode
+				.SelectSingleNode("//html/body/table/tbody/tr/td/div/div/div[@id='search']");
+		}
+
+		static HtmlNode SearchCardNode(HtmlNode searchNode) {
+			return searchNode
+				.SelectSingleNode("./div/ol/div/div/div");
+		}
+
+		static string FirstEquals(string txt) {
+			string[] lines = Lines(txt);
+			foreach(string line in lines) {
+				if(line.Contains(" = ") || line.Contains("equals")) {
+					return line;
+				}
+			}
+			return null as string;
+		}
+
+		static void Main(string[] args)
+		{
+			string encoded = HttpUtility.UrlEncode(
+				String.Join(" ", args)
+			);
+			string url = "https://google.com/search?q=" + encoded;
+			HtmlWeb web = new HtmlWeb();
+			var html = web.Load(url);
+			// looking for:
+			// div#search > div > ol > div > div > div
+			// the built-in google search "card"
+			// yikes!!
+			var search = SearchNode(html);
+			var card = SearchCardNode(search);
+			if(card != null && card.InnerText.Contains(" = ")) {
+				Console.WriteLine(card.InnerText);
+			} else {
+				// uh oh...
+				Console.WriteLine(
+					"WARNING: No Google conversion found!"
+				);
+				string conversion = FirstEquals(search.InnerText);
+				if(conversion != null) {
+					Console.WriteLine(conversion);
+				} else {
+					Console.WriteLine(
+						"ERROR: No conversions found!"
+					);
+				}
+			}
+		}
+	}
+}
