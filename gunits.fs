46e36506d9f028143d9e1f20e192e2a4b8c4a38f
Rebecca Turner <637275@gmail.com>
2017-12-17 03:23:25 -0500

    fsharp

--- gunits.fs
+++ gunits.fs
@@ -1,1 +1,8 @@
-printf "hello world"
+open FSharp.Data
+open System
+
+[<EntryPoint>]
+let main argv =
+    let doc = HtmlDocument.Load("https://google.com/search?q=FSharp.Data")
+    printf "%s" doc
+    0
